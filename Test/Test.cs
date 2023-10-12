acc255dbd8a9a85765f026253609383b2df9191e
Tomas Zouhar <xzouha13@vutbr.cz>
2023-10-12 20:16:00 +0200

    Created testing project

---
+++ Test/Test.cs
@@ -0,0 +1,12 @@
+using System.Text;
+using System.Text.Json;
+using OrderEventProcessor.Database;
+using OrderEventProcessor.Model;
+using RabbitMQ.Client;
+
+namespace OrderEventProcessor.Tests;
+
+[TestFixture]
+public class Test
+{
+}
